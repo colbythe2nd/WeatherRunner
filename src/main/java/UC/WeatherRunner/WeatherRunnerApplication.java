ca3db83b564c9e1aa9620f91dcf0794aacace8fd
Trevor C <tscromwell@live.com>
2021-10-21 09:03:29 -0400

    Generated project with SpringInitializr

---
+++ src/main/java/UC/WeatherRunner/WeatherRunnerApplication.java
@@ -0,0 +1,13 @@
+package UC.WeatherRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRunnerApplication.class, args);
+	}
+
+}
