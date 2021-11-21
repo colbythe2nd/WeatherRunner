c329332d34b28997c97c8d327e5fba2c81dbc856
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2021-11-21 13:54:39 -0500

    creates shell of service impl

---
+++ src/main/java/UC/WeatherRunner/service/RunServiceImpl.java
@@ -0,0 +1,33 @@
+package UC.WeatherRunner.service;
+
+import java.util.List;
+
+import UC.WeatherRunner.entity.RunEntity;
+
+public class RunServiceImpl implements RunService {
+
+	@Override
+	public List<RunEntity> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RunEntity findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(RunEntity theRun) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
